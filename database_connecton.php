75a432a3eba96f4cf92f7ee00104ecb0ad7b7334
skmirajbn <skmirajbn@gmail.com>
2023-06-03 20:24:37 +0600

    updated

---
+++ database_connecton.php
@@ -0,0 +1,10 @@
+<?php 
+
+$dbhost = 'skmiraj.online';
+$dbusername = 'skmiraj';
+$dbpass = '6XfwV(z{zfc!P38H';
+$dbname = 'skmiraj_storemanagement';
+
+$connect = new mysqli($dbhost,$dbusername,$dbpass,$dbname);
+session_start();
+?>
