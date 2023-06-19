6f9c64ec4045c24339eeb0daee5afa3b44bfca31
skmirajbn <skmirajbn@gmail.com>
2023-06-19 10:04:21 +0600

    updated

--- Admin Project/pages/all_subCategories.php
+++ Admin Project/pages/all_subCategories.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 require_once("../includes/db_connection.php");
 
 //pagination
@@ -23,22 +23,29 @@ $result = $con->query($sql);
         <th><input type="checkbox" class="select-all"></th>
         <th>Sub Category ID</th>
         <th>Sub Category Name</th>
+        <th>Category Name</th>
         <th>Actions</th>
       </tr>
     </thead>
     <tbody>
-      <?php 
-                    while($data = $result->fetch_assoc()){
-                    ?>
-      <tr>
-        <td><input type="checkbox" class="select"></td>
-        <td><?=$data['sub_category_id']?></td>
-        <td><?=$data['sub_category_name']?></td>
-        <td>
-          <a href="pages/edit_subCategory.php?subCategory_id=<?=$data['sub_category_id']?>" class="edit-btn btn"><i class="fa-solid fa-pen-to-square"></i></a>
-          <a href="formServer.php?subCategoryDelete=<?=$data['sub_category_id']?>" class="delete-btn btn"><i class="fa-solid fa-trash"></i></a>
-        </td>
-      </tr>
+      <?php
+      while ($data = $result->fetch_assoc()) {
+        ?>
+        <tr>
+          <td><input type="checkbox" class="select"></td>
+          <td>
+            <?= $data['sub_category_id'] ?>
+          </td>
+          <td>
+            <?= $data['sub_category_name'] ?>
+          </td>
+          <td>
+            <a href="pages/edit_subCategory.php?subCategory_id=<?= $data['sub_category_id'] ?>" class="edit-btn btn"><i
+                class="fa-solid fa-pen-to-square"></i></a>
+            <a href="formServer.php?subCategoryDelete=<?= $data['sub_category_id'] ?>" class="delete-btn btn"><i
+                class="fa-solid fa-trash"></i></a>
+          </td>
+        </tr>
       <?php } ?>
       <!-- Add more rows for each user -->
     </tbody>
@@ -47,23 +54,23 @@ $result = $con->query($sql);
 
 <div class="pagination">
   <a href="pages/all_categories.php?page=1">&laquo;</a>
-  <a href="pages/all_categories.php?page=<?=$currentPage-1?>">&lt;</a>
-  <?php 
-                if($currentPage>2){
-                  $page = $currentPage - 2;
-                  $lastPage = $currentPage + 2;
-                }else{
-                  $page = 1;
-                  $lastPage = 5;
-                }
-                if($lastPage>=$totalPage){
-                  $lastPage = $totalPage;
-                }
-                for($page; $page<=$lastPage; $page++){ 
-                  ?>
-  <a href="pages/all_categories.php?page=<?=$page?>" class="<?=$currentPage == $page ? 'active' : '' ?>"><?=$page?></a>
+  <a href="pages/all_categories.php?page=<?= $currentPage - 1 ?>">&lt;</a>
+  <?php
+  if ($currentPage > 2) {
+    $page = $currentPage - 2;
+    $lastPage = $currentPage + 2;
+  } else {
+    $page = 1;
+    $lastPage = 5;
+  }
+  if ($lastPage >= $totalPage) {
+    $lastPage = $totalPage;
+  }
+  for ($page; $page <= $lastPage; $page++) {
+    ?>
+    <a href="pages/all_categories.php?page=<?= $page ?>" class="<?= $currentPage == $page ? 'active' : '' ?>"><?= $page ?></a>
   <?php } ?>
-  <a href="pages/all_categories.php?page=<?=$currentPage + 1?>">&gt;</a>
-  <a href="pages/all_categories.php?page=<?=$totalPage?>">&raquo;</a>
+  <a href="pages/all_categories.php?page=<?= $currentPage + 1 ?>">&gt;</a>
+  <a href="pages/all_categories.php?page=<?= $totalPage ?>">&raquo;</a>
 </div>
 </div>
