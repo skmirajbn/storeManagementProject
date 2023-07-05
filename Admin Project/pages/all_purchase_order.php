b9537503d042684e48279019f2918395754e137e
skmirajbn <skmirajbn@gmail.com>
2023-07-05 10:34:52 +0600

    updated

--- Admin Project/pages/all_purchase_order.php
+++ Admin Project/pages/all_purchase_order.php
@@ -5,14 +5,14 @@ require_once("../includes/db_connection.php");
 $limit = 13;
 $currentPage = $_GET['page'] ?? 1;
 
-$countSql = "SELECT COUNT(*) AS TOTAL FROM products";
+$countSql = "SELECT COUNT(*) AS TOTAL FROM purchase_order";
 $result = $con->query($countSql);
 $totalRows = $result->fetch_assoc()['TOTAL'];
 $totalPage = ceil($totalRows / $limit);
 $offset = ($currentPage - 1) * $limit;
 
 
-$sql = "SELECT * FROM sales_order_information WHERE sales_order_status = 1 ORDER by sales_order_id DESC LIMIT $offset, $limit";
+$sql = "SELECT * FROM purchase_order_information WHERE purchase_order_status = 1 ORDER by purchase_order_id DESC LIMIT $offset, $limit";
 $result = $con->query($sql);
 
 ?>
@@ -21,9 +21,9 @@ $result = $con->query($sql);
         <thead>
             <tr>
                 <th><input type="checkbox" class="select-all"></th>
-                <th>Sales Order ID</th>
-                <th>Customer Name</th>
-                <th>Sales Man</th>
+                <th>Purchase Order ID</th>
+                <th>Supplier Name</th>
+                <th>User</th>
                 <th>Date and Time</th>
                 <th>Invoice</th>
             </tr>
@@ -35,10 +35,10 @@ $result = $con->query($sql);
                 <tr>
                     <td><input type="checkbox" class="select"></td>
                     <td>
-                        <?= $data['sales_order_id'] ?>
+                        <?= $data['purchase_order_id'] ?>
                     </td>
                     <td>
-                        <?= $data['customer_name'] ?>
+                        <?= $data['supplier_name'] ?>
                     </td>
                     <td>
                         <?= $data['user_name'] ?>
@@ -48,7 +48,7 @@ $result = $con->query($sql);
                     </td>
                     <td>
                         <a class="btn mybtn-hightlight d-block p-0"
-                            href="pages/sales_orderInvoice.php?sales_order_id=<?= $data['sales_order_id'] ?>">View
+                            href="pages/purchase_orderInvoice.php?purchase_order_id=<?= $data['purchase_order_id'] ?>">View
                             Invoice
                             <i class="fa-solid fa-file-invoice"></i></a>
                     </td>
@@ -60,8 +60,8 @@ $result = $con->query($sql);
 </div>
 
 <div class="pagination">
-    <a href="pages/all_salesOrder.php?page=1">&laquo;</a>
-    <a href="pages/all_salesOrder.php?page=<?= $currentPage - 1 ?>">&lt;</a>
+    <a href="pages/all_purchase_order.php?page=1">&laquo;</a>
+    <a href="pages/all_purchase_order.php?page=<?= $currentPage - 1 ?>">&lt;</a>
     <?php
     if ($currentPage > 2) {
         $page = $currentPage - 2;
@@ -75,9 +75,9 @@ $result = $con->query($sql);
     }
     for ($page; $page <= $lastPage; $page++) {
         ?>
-        <a href="pages/all_salesOrder.php?page=<?= $page ?>" class="<?= $currentPage == $page ? 'active' : '' ?>"><?= $page ?></a>
+        <a href="pages/all_purchase_order.php?page=<?= $page ?>" class="<?= $currentPage == $page ? 'active' : '' ?>"><?= $page ?></a>
     <?php } ?>
-    <a href="pages/all_salesOrder.php?page=<?= $currentPage + 1 ?>">&gt;</a>
-    <a href="pages/all_salesOrder.php?page=<?= $totalPage ?>">&raquo;</a>
+    <a href="pages/all_purchase_order.php?page=<?= $currentPage + 1 ?>">&gt;</a>
+    <a href="pages/all_purchase_order.php?page=<?= $totalPage ?>">&raquo;</a>
 </div>
 </div>
