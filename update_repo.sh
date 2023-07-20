bdbac97a09859bbafaa61234a90f4b5d74bd49c8
skmirajbn <skmirajbn@gmail.com>
2023-07-21 01:30:52 +0600

    updated

---
+++ update_repo.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Navigate to the project directory
+cd /c/websites/skmiraj/storeManagementProject
+
+# Reset any changes in the repository
+git reset --hard HEAD
+
+# Pull the latest changes from the master branch
+git pull origin master
