897538df7b8bcbb6549323c823f2669b00a267da
skmirajbn <skmirajbn@gmail.com>
2023-07-26 18:07:11 +0600

    updated

--- Admin Project/includes/db_connection.php
+++ Admin Project/includes/db_connection.php
@@ -1,11 +1,11 @@
-<?php 
+<?php
 $host = "skmiraj.online";
 $dbuser = "skmiraj";
 $dbpass = "6XfwV(z{zfc!P38H";
 $dbname = "skmiraj_storemanagement";
 
 $con = new mysqli($host, $dbuser, $dbpass, $dbname);
-if($con->connect_error){
+if ($con->connect_error) {
     die("");
 }
 ?>
